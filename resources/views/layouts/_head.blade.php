fdb211e0938e544cb6e037423df1e253e52fa8df
zhujw <zhujianghan@gmail.com>
2018-09-11 14:30:22 +0000

    Move user model to models folder

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -18,12 +18,18 @@
 <body>
 
 <!-- Fixed navbar -->
-<nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
             <a class="navbar-brand" href="#">Sample App</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
+        <div id="navbar" class=" navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li><a href="{{ route('home') }}">首页</a></li>
                 <li><a href="{{ route('about') }}">关于</a></li>
@@ -41,12 +47,11 @@
                     </ul>
                 </li>
             </ul>
-
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">登陆</a></li>
                 <li><a href="#">注册</a></li>
             </ul>
-        </div><!--/.nav-collapse -->
+        </div>
     </div>
 </nav>
 
