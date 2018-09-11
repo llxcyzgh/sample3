6ba770742e0356b0a6ef82d1f295e51d36d0decf
zhujw <zhujianghan@gmail.com>
2018-09-11 23:16:49 +0000

    finish Model-User

--- resources/views/layouts/_foot.blade.php
+++ resources/views/layouts/_foot.blade.php
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="clearfix">
             <div class="pull-left"><img src="https://a.photo/images/2018/09/11/favicon-jquery_.th.png" alt=""
-                                        width="13"> write less, do more
+                                        width="13"> Write less, do more.  Convention over configuration
             </div>
             <div class="pull-right">&copy; 2016 Company, Inc.</div>
         </div>
